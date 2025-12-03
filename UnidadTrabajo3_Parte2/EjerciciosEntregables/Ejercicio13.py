e563440410f3d7ec144e22691bb256db8e35eb92
SaternDev <saterndev@gmail.com>
2025-12-03 08:25:08 +0100

    feat(entregable-13): completar implementación del decimotercer entregable

--- UnidadTrabajo3_Parte2/EjerciciosEntregables/Ejercicio13.py
+++ UnidadTrabajo3_Parte2/EjerciciosEntregables/Ejercicio13.py
@@ -1,4 +1,5 @@
-# Recorre el directorio actual y muestra el archivo con fecha de modificación más reciente. Para ello, investiga la función stat() que puedes aplicar sobre los archivos.
+# Recorre el directorio actual y muestra el archivo con fecha de modificación más reciente.
+#  Para ello, investiga la función stat() que puedes aplicar sobre los archivos.
 
 # Si no hay archivos, muestra “Sin archivos”.
 
@@ -6,9 +7,25 @@
 
 from pathlib import Path
 
-if Path.cwd().stat().st_size() == 0:
-    print('Sin archivos')
+# if Path.cwd().stat().st_size() == 0:
+#     print('Sin archivos')
+
+modTime = 0.
+fileName = ''
+hasFiles = False
 
 for i in Path.cwd().iterdir():
-    print(i.stat())
-    print(i)
+    if i.is_file():   
+        if not hasFiles:
+            hasFiles = True
+
+        modTimeFile = i.stat().st_mtime
+        print(modTimeFile)
+        if modTimeFile > modTime:
+            modTime = modTimeFile
+            fileName = str(i)
+            
+if hasFiles: 
+    print(f'El archivo más reciente es: {fileName}')
+else:
+    print('Sin archivos')
