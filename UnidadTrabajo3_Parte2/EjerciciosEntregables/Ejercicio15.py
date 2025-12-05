5bc9da4a6474391d71dd693c51ac72c2c3cd49e7
SaternDev <saterndev@gmail.com>
2025-12-05 09:28:58 +0100

    feat(Ejercicio-15): complete implementation of deliverable 15

--- UnidadTrabajo3_Parte2/EjerciciosEntregables/Ejercicio15.py
+++ UnidadTrabajo3_Parte2/EjerciciosEntregables/Ejercicio15.py
@@ -7,3 +7,26 @@
 
 # Librerías: from pathlib import Path
 
+from pathlib import Path
+
+countLog = 0
+totalLog = 0
+
+numAulas = int(input('Dime el número de aulas: '))
+numPc = int(input('Dime el número de PCs por aula: '))
+currentDir = Path.cwd()
+
+for i in range(1, numAulas + 1):
+    aulaDir = Path(str(currentDir) + '/' + "AULA-" + str(i).zfill(2))
+    if aulaDir.is_dir():
+        for b in range(1, numPc + 1):
+            pcDir = Path(str(aulaDir) + '/' + "PC-" + str(b).zfill(2))
+            if pcDir.is_dir():
+                for c in pcDir.iterdir():
+                    if str(c.name).endswith('.log'):
+                        countLog += 1
+                        totalLog += 1
+                print(f'En PC-{str(b).zfill(2)} hay un total de {countLog} archivos ".log"')
+                countLog = 0
+
+print(f'El total de archivos ".log" es {totalLog}')
